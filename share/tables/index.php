9264bd7ae3f17b325a445ed4d21afd47b8ad9f06
Yannick Charton <tontonitch-pro@yahoo.fr>
2011-07-19 19:14:46 +0200

    cleaned a bit the interface selection

--- share/tables/index.php
+++ share/tables/index.php
@@ -14,26 +14,38 @@ function filename( $filename ) {
     }
 }
 
+function nodename( $filename ) {
+    $filename = filename( $filename );
+    //$filename = preg_replace("/(Q..)/", pack("H2",substr("$1",1,3)), $filename);
+    //temporary basic decoding
+    $filename = preg_replace("/(Q2D)/", "-", $filename);
+    $filename = preg_replace("/(Q2E)/", ".", $filename);
+    $filename = preg_replace("/(Q51)/", "Q", $filename);
+    if (($res = strrpos($filename, '-Interface')) !== FALSE) {
+        return substr($filename, 0, $res);
+    } else {
+        return $filename;
+    }
+}
+
 $dirname = './';
 $dir = opendir($dirname);
 
 while($file = readdir($dir)) {
-        if($file != '.' && $file != 'index.php' && $file != '..' && !is_dir($dirname.$file))
-        {
-                echo '<option value='.$file.'>'.filename($file).'</option>';
-        }
+    if($file != '.' && $file != 'index.php' && $file != '.index.php.swp' && $file != '..' && !is_dir($dirname.$file)) {
+        echo '<option value='.$file.'>'.nodename($file).'</option>';
+    }
 }
 
 closedir($dir);
 ?>
 
 </select>
-<input type="submit" value="validate" name="ok">
+<input type="submit" value="go" name="go">
 </form><br>
 
 <?php
-if (isset($_GET['nodename']))
-{
+if (isset($_GET['nodename'])) {
     $nodename=$_GET['nodename'];
     include("$nodename");
 }
