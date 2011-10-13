cc333266c05e059e53e356d105e824bbb530a101
Yannick Charton <tontonitch-pro@yahoo.fr>
2011-10-13 17:34:33 +0200

    index.php selection list is now alphab. ordered

--- share/tables/index.php
+++ share/tables/index.php
@@ -30,11 +30,15 @@ function nodename( $filename ) {
 
 $dirname = './';
 $dir = opendir($dirname);
-
 while($file = readdir($dir)) {
     if($file != '.' && $file != 'index.php' && $file != '.index.php.swp' && $file != '..' && !is_dir($dirname.$file)) {
-        echo '<option value='.$file.'>'.nodename($file).'</option>';
+        #echo '<option value='.$file.'>'.nodename($file).'</option>';
+        $array_files[$file] = nodename($file);
     }
+}
+asort($array_files);
+foreach ($array_files as $key => $val) {
+    echo '<option value='.$key.'>'.$val.'</option>';
 }
 
 closedir($dir);
