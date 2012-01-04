5c149feee3c13725934a343fda9a003889d0f22d
Yannick Charton <tontonitch-pro@yahoo.fr>
2012-01-04 23:42:45 +0100

    added a snmp oid recorder for device simulation purpose (for testing phase). win32 precompiled for easy use, and howto

---
+++ contrib/support/win32/take_record.bat
@@ -0,0 +1,28 @@
+@ECHO off
+SET AGENT_ADDRESS=%1
+SET AGENT_COMMUNITY=%2
+SET DEVICE_NAME=%3
+
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.2.1.1.1 --stop-oid=1.3.6.1.2.1.1.6 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.2.1.2.2.1 --stop-oid=1.3.6.1.2.1.2.2.2 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.2.1.4.20.1.2 --stop-oid=1.3.6.1.2.1.4.20.1.4 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.4.1.9.5.1.2 --stop-oid=1.3.6.1.4.1.9.5.1.3 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.4.1.9.5.1.4.1.1 --stop-oid=1.3.6.1.4.1.9.5.1.4.1.2 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.4.1.9.9.68.1.2.2.1 --stop-oid=1.3.6.1.4.1.9.9.68.1.2.2.2 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.2.1.10.7.2.1.19 --stop-oid=1.3.6.1.2.1.10.7.2.1.20 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.4.1.11.2.14.11.5.1.7.1.15.3.1.2 --stop-oid=1.3.6.1.4.1.11.2.14.11.5.1.7.1.15.3.1.3 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.2.1.17.1 --stop-oid=1.3.6.1.2.1.17.3 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.2.1.31.1.1.1 --stop-oid=1.3.6.1.2.1.31.1.1.2 --output-file=devices/%DEVICE_NAME%.rec
+echo/|set /p =.
+snmprec.exe --quiet --agent-address=%AGENT_ADDRESS% --agent-port=161 --v2c --community=%AGENT_COMMUNITY% --start-oid=1.3.6.1.2.1.47.1.2.1.1.2 --stop-oid=1.3.6.1.2.1.47.1.2.1.1.3 --output-file=devices/%DEVICE_NAME%.rec
+
+ECHO Done!
